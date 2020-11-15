08ed0611ea6910352d04d3e928f8f6010701e3a0
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-11-14 21:59:17 -0500

    added visitor model-class

---
+++ COMP306-Project-Backend/Models/Visitor.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP306_Project_Backend.Models
+{
+
+    [DynamoDBTable("Visitors")]
+    public class Visitor
+    {
+        [DynamoDBHashKey]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public List<string> PlacesVisited { get; set; }
+
+    }
+}

b643fd209d722cd18bea20b278f64d4aa93f02fa
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-11-17 10:43:46 -0500

    address & table operations classes created with createtable tasks

---
+++ COMP306-Project-Backend/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP306_Project_Backend.Models
+{
+    [DynamoDBTable("Address")]
+    public class Address
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        public string AddressLineOne { get; set; }
+        public string AddressLineTwo { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Province { get; set; }
+
+    }
+}
---
+++ COMP306-Project-Backend/Models/TableOperations.cs
@@ -0,0 +1,65 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace COMP306_Project_Backend.Models
+{
+    public static class TableOperations
+    {
+
+        public static async Task CreateVisitorTable(AmazonDynamoDBClient client)
+        {
+
+            await Task.Run(() =>
+            {
+                client.CreateTableAsync(new CreateTableRequest
+                {
+                    TableName= "Visitors",
+                    ProvisionedThroughput= new ProvisionedThroughput { ReadCapacityUnits=5,WriteCapacityUnits=5},
+                    KeySchema=new List<KeySchemaElement>
+                    {
+                        new KeySchemaElement
+                        {
+                            AttributeName="Email",
+                            KeyType=KeyType.HASH
+                        }
+                    },
+                    AttributeDefinitions= new List<AttributeDefinition>
+                    {
+                        new AttributeDefinition{AttributeName="Email",AttributeType=ScalarAttributeType.S}
+                    }
+
+                });
+                Thread.Sleep(5000);
+            });
+        }
+
+        public static async Task CreateAddressTable(AmazonDynamoDBClient client)
+        {
+            await Task.Run(() =>
+            {
+                client.CreateTableAsync(new CreateTableRequest
+                {
+                    TableName = "Address",
+                    ProvisionedThroughput = new ProvisionedThroughput { ReadCapacityUnits = 5, WriteCapacityUnits = 5 },
+                    KeySchema = new List<KeySchemaElement>
+                    {
+                        new KeySchemaElement
+                        {
+                            AttributeName="Id",
+                            KeyType=KeyType.HASH
+                        }
+                    },
+                    AttributeDefinitions= new List<AttributeDefinition>
+                    {
+                        new AttributeDefinition{AttributeName="Id",AttributeType=ScalarAttributeType.S}
+                    }
+                });
+            });
+        }
+    }
+}

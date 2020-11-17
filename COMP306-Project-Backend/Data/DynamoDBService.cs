8ff3651ce9e2d062918df65abae0a99e73a90844
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-11-17 16:21:35 -0500

    added DynamoDBService

---
+++ COMP306-Project-Backend/Data/DynamoDBService.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP306_Project_Backend.Data
+{
+    public class DynamoDBService
+    {
+        IAmazonDynamoDB dynamoDBClient { get; set; }
+        AmazonDynamoDBClient client;
+        DynamoDBContext context;
+
+        public DynamoDBService(IAmazonDynamoDB dynamoDBClient)
+        {
+            this.dynamoDBClient = dynamoDBClient;
+        }
+
+    }
+}

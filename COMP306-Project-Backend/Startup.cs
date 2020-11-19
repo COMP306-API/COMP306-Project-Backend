4c58cc686a60bb4393139726aad431693068c04d
Logan Kim <logan.kim0212@gmail.com>
2020-11-18 22:17:32 -0500

    Automapper added but commented for now

--- COMP306-Project-Backend/Startup.cs
+++ COMP306-Project-Backend/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,6 +31,8 @@ namespace COMP306_Project_Backend
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
             services.AddAWSService<IAmazonDynamoDB>();
+
+            //services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

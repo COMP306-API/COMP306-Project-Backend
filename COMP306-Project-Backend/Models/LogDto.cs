e4a558dc9fd4d70c246b9674c9ba697f16480b26
Logan Kim <logan.kim0212@gmail.com>
2020-11-18 22:59:32 -0500

    LogDto and UserDto created

---
+++ COMP306-Project-Backend/Models/LogDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP306_Project_Backend.Models
+{
+    public class LogDto
+    {
+        public string BusinessEmail { get; set; }
+        public string DateVisited { get; set; }
+        public string TimeVisited { get; set; }
+        public string ClientEmail { get; set; }
+
+        public LogDto(
+            string BusinessEmail,
+            string DateVisited,
+            string TimeVisited,
+            string ClientEmail
+            )
+        {
+            this.BusinessEmail = BusinessEmail;
+            this.DateVisited = DateVisited;
+            this.TimeVisited = TimeVisited;
+            this.ClientEmail = ClientEmail;
+        }
+    }
+}
---
+++ COMP306-Project-Backend/Models/UserDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP306_Project_Backend.Models
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public UserDto(
+            string Email,
+            string Name,
+            string Address,
+            string City,
+            string Province,
+            string PostalCode,
+            string PhoneNumber
+            )
+        {
+            this.Email = Email;
+            this.Name = Name;
+            this.Address = Address;
+            this.City = City;
+            this.Province = Province;
+            this.PostalCode = PostalCode;
+            this.PhoneNumber = PhoneNumber;
+        }
+    }
+}
